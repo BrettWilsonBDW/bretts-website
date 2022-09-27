ef367cec3db03c9b5905b40e4c1d8380d749315a
TonicComa37 <brett.david.wilson.bdw@outlook.com>
2022-09-27 16:10:26 +0200

    rolled back to the previous commit

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded",(function(event){fetch("../assets/shared/nav.html").then(res=>res.text()).then(text=>{let oldelem=document.querySelector("script#replace_with_navbar"),newelem=document.createElement("div");newelem.innerHTML=text,oldelem.parentNode.replaceChild(newelem,oldelem)})}));
+fetch('../assets/shared/nav.html').then(res => res.text()).then(text => {let oldelem = document.querySelector("script#replace_with_navbar");let newelem = document.createElement("div");newelem.innerHTML = text;oldelem.parentNode.replaceChild(newelem,oldelem);})
